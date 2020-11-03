738ea428a301f91b11c3a44aceea6a3e8796f38e
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2020-11-03 20:54:51 +0000

    extended test case

--- acceptance-tests/buffer_size_test.go
+++ acceptance-tests/buffer_size_test.go
@@ -9,4 +9,15 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("max_rewr
+var _ = Describe("max_rewrite and buffer_size_bytes", func() {
+	It("Allows HTTP requests as large as buffer_size_bytes - max_rewrite", func() {
+		opsfileBufferSize := `---
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/max_rewrite?
+  value: 4096
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/buffer_size_bytes?
+  value: 71000
+`
+		haproxyBackendPort := 12000
+		haproxyInfo, _ := deployHAProxy(baseManifestV

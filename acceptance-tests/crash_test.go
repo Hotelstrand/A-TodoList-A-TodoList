1062a3c5a331ac34685fc23634762ae0a3df1523
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2020-11-23 22:23:35 +0000

    x32: Use compat shims for {g,s}etsockopt

--- acceptance-tests/crash_test.go
+++ acceptance-tests/crash_test.go
@@ -12,4 +12,15 @@ import (
 
 var _ = Describe("Crash Test", func() {
 	opsfileDrainTimeout := `---
-# Enable
+# Enable health check
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/enable_health_check_http?
+  value: true
+# Enable draining
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/drain_enable?
+  value: true
+# Set grace period to 1s
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/drain_frontend_grace_time?
+  value:

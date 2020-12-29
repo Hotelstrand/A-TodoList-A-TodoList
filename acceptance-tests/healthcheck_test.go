30b82f0da651f677ee01e4309061a89a945057fa
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2020-12-29 06:36:15 +0000

    6.2

--- acceptance-tests/healthcheck_test.go
+++ acceptance-tests/healthcheck_test.go
@@ -3,4 +3,21 @@ package acceptance_tests
 import (
 	"fmt"
 	"io"
-	"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("HTTP Health Check", func() {
+	opsfileHTTPHealthcheck := `---
+# Enable health check
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/enable_health_check_http?
+  value: true
+`
+
+	It("Correctly fails to start if there is no healthy backend", func(

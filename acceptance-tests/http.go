701b513b4bca86a74167bf561aa48abae892298f
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-01-16 09:08:22 +0000

    See Changelog.

--- acceptance-tests/http.go
+++ acceptance-tests/http.go
@@ -13,4 +13,13 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/n
+	"golang.org/x/net/http2"
+)
+
+// starts a local http server handling the provided handler
+// returns a close function to stop the server and the port the server is listening on
+func startLocalHTTPServer(tlsConfig *tls.Config, handler func(http.ResponseWriter, *http.Request)) (func(), int, error) {
+	server := httptest.NewUnstartedServer(http.HandlerFunc(handler))
+	if tlsConfig != nil {
+		server.TLS = tlsConfig
+		server.S

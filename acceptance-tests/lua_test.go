1e78efc9c47f05370406cbb3ef919d33e65f05db
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-02-01 16:53:26 +0000

    Fixed #54

--- acceptance-tests/lua_test.go
+++ acceptance-tests/lua_test.go
@@ -11,4 +11,15 @@ import (
 )
 
 var _ = Describe("Lua scripting", func() {
-	It("Deploys haproxy with lua scrip
+	It("Deploys haproxy with lua script", func() {
+		opsfileLua := `---
+# Enable Lua scripting
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/lua_scripts?/-
+  value: "/var/vcap/packages/haproxy/lua_test.lua"
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/frontend_config?
+  value: "http-request use-service lua.lua_test if { path /lua_test }"
+`
+
+		replyLuaCo

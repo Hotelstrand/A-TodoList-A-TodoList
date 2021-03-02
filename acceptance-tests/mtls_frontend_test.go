35b730807051b2c217384644e4fb248b7a6d10cd
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-03-02 14:56:15 +0000

    Update documentation/IntelBasedGatewaySolutions.md

--- acceptance-tests/mtls_frontend_test.go
+++ acceptance-tests/mtls_frontend_test.go
@@ -2,4 +2,23 @@ package acceptance_tests
 
 import (
 	"crypto/tls"
-	"fmt
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("mTLS", func() {
+	var haproxyInfo haproxyInfo
+	var closeTunnel func()
+	var closeLocalServer func()
+
+	haproxyBackendPort := 12000
+	opsfileMTLS := `---
+# Configure CA and cert chain
+- type: replace
+  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/crt_list?
+  value:
+  - snifilter:
+    - a.haproxy.internal
+   

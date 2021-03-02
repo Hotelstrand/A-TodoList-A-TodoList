ff487f1c863212e13ea3e3648623a38784e3790c
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-03-02 21:27:30 +0000

    bump to 0.2.2

--- acceptance-tests/mtls_frontend_test.go
+++ acceptance-tests/mtls_frontend_test.go
@@ -32,4 +32,25 @@ var _ = Describe("mTLS", func() {
     verify: required
     ssl_pem:
       cert_chain: ((https_frontend.certificate))((https_frontend_ca.certificate))
-      private_key: ((https_frontend.priva
+      private_key: ((https_frontend.private_key))
+# Declare server certs
+- type: replace
+  path: /variables?/-
+  value:
+    name: https_frontend_ca
+    type: certificate
+    options:
+      is_ca: true
+      common_name: bosh
+- type: replace
+  path: /variables?/-
+  value:
+    name: https_frontend
+    type: certificate
+    options:
+      ca: https_frontend_ca
+      common_name: a.haproxy.internal
+      alternative_names:
+      - a.haproxy.internal
+      - b.haproxy.internal
+# De

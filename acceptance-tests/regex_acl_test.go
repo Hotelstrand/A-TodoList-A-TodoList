f7ab183b4a769dcceab27d46e1a691628e63b00a
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-05-05 08:29:47 +0000

    Defaults

--- acceptance-tests/regex_acl_test.go
+++ acceptance-tests/regex_acl_test.go
@@ -14,4 +14,13 @@ var _ = Describe("Regex-based ACLs", func() {
 - type: replace
   path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/frontend_config?
   value: |-
-    acl i
+    acl is_regex_match url,debug -i -m reg foo
+    http-request deny deny_status 429 if is_regex_match
+    http-request deny deny_status 401 if ! is_regex_match
+`
+		haproxyBackendPort := 12000
+		haproxyInfo, _ := deployHAProxy(baseManifestVars{
+			haproxyBackendPort:    haproxyBackendPort,
+			haproxyBackendServers: []string{"127.0.0.1"},
+			deploymentName:        deploymentNameForTestNode(),
+		}, []string{opsfileRegexACLs}, map[string]interface{}{}

844d2f38ce2a89aded4a15cccb0aa44cd82093af
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-05-31 11:25:34 +0000

    Update LuCI-feed to yesterdays HEAD of master

--- acceptance-tests/websocket_test.go
+++ acceptance-tests/websocket_test.go
@@ -40,4 +40,26 @@ var _ = Describe("HTTPS Frontend", func() {
     snifilter:
     - haproxy.internal
     ssl_pem:
-      cert_chain: ((https_frontend.certificate))((https_frontend_ca.certificat
+      cert_chain: ((https_frontend.certificate))((https_frontend_ca.certificate))
+      private_key: ((https_frontend.private_key))
+# Declare certs
+- type: replace
+  path: /variables?/-
+  value:
+    name: https_frontend_ca
+    type: certificate
+    options:
+      is_ca: true
+      common_name: bosh
+- type: replace
+  path: /variables?/-
+  value:
+    name: https_frontend
+    type: certificate
+    options:
+      ca: https_frontend_ca
+      common_name: haproxy.internal
+      alternative_names: [haproxy.internal]
+`
+
+	var creds s

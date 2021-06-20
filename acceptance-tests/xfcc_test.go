4d6c85dc53a68b9ec40ca196abd3553e16e5b800
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2021-06-20 18:36:08 +0000

    v1.3.6

--- acceptance-tests/xfcc_test.go
+++ acceptance-tests/xfcc_test.go
@@ -9,4 +9,25 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/base64"
-	"enc
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"net/http"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+type Certificate struct {
+	CertPEM       string
+	PrivateKeyPEM string
+	X509Cert      *x509.Certificate
+	PrivateKey    *rsa.PrivateKey
+	TLSCert       tls.Certificate
+}
+
+/*
+	https://bosh.io/jobs/haproxy?source=github.com/cloudfoundry-community/haproxy-boshrelease#p%3dha_proxy.forwarded_client_cert
+	forwarded_client_cert
+		always_forward_onl

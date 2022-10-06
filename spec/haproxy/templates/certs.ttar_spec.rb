41ca9b6c0837343735612d1105c0ec5917d6853c
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-10-06 20:56:17 +0000

    Minor comments

--- spec/haproxy/templates/certs.ttar_spec.rb
+++ spec/haproxy/templates/certs.ttar_spec.rb
@@ -1,1 +1,21 @@
-# frozen_string_
+# frozen_string_literal: true
+
+require 'rspec'
+
+describe 'config/certs.ttar' do
+  let(:template) { haproxy_job.template('config/certs.ttar') }
+
+  describe 'ha_proxy.ssl_pem' do
+    let(:ttar) do
+      template.render({
+        'ha_proxy' => {
+          'ssl_pem' => ssl_pem
+        }
+      })
+    end
+
+    context 'when ssl_pem is an array of objects' do
+      let(:ssl_pem) do
+        [{
+          'cert_chain' => 'cert_chain 0 contents',
+          'private_key' => 'private_k

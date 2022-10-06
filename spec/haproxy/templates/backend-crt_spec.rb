b9d7bad7b53e5677872e2630c1e75873e502c684
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-10-06 17:45:28 +0000

    increment version number

--- spec/haproxy/templates/backend-crt_spec.rb
+++ spec/haproxy/templates/backend-crt_spec.rb
@@ -1,20 +1,21 @@
+
 # frozen_string_literal: true
 
 require 'rspec'
 
-describe 'config/backend-ca-certs.pem' do
-  let(:template) { haproxy_job.template('config/backend-ca-certs.pem') }
+describe 'config/backend-crt.pem' do
+  let(:template) { haproxy_job.template('config/backend-crt.pem') }
 
-  describe 'ha_proxy.backend_ca_file' do
+  describe 'ha_proxy.backend_crt' do
     it 'has the correct contents' do
       expect(template.render({
         'ha_proxy' => {
-          'backend_ca_file' => 'foobarbaz'
+          'backend_crt' => 'foobarbaz'
         }
       })).to eq("\nfoobarbaz\n\n")
     end
 
-    context 'when ha_proxy.backend_ca_file is not provided' do
+    context 'when ha_proxy.backend_crt is not provided' do
       it 'is empty' do
         expect(template.render({})).to be_a_blank_string
       end

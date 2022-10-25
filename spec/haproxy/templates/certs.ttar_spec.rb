aa6a1b1b6decf914f5409eed438512103ae7a3d3
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-10-25 20:17:58 +0000

    Fix the build

--- spec/haproxy/templates/certs.ttar_spec.rb
+++ spec/haproxy/templates/certs.ttar_spec.rb
@@ -355,3 +355,22 @@ describe 'config/certs.ttar' do
             'ssl_pem' => 'ssl_pem contents'
           }]
         }
+      })
+    end
+
+    it 'is included in the crt list' do
+      expect(ttar_entry(ttar, '/var/vcap/jobs/haproxy/config/ssl/crt-list')).to eq(<<~EXPECTED)
+
+        /var/vcap/jobs/haproxy/config/ssl/cert-0.pem [ssl-min-ver TLSv1.2]
+
+
+      EXPECTED
+    end
+  end
+
+  describe 'ha_proxy.crt_list[].ssl_max_version' do
+    let(:ttar) do
+      template.render({
+        'ha_proxy' => {
+          'crt_list' => [{
+            'ssl_max_version' => 

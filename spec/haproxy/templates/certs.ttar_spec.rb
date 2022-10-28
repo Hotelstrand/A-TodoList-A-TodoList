3f37a2096f9a976cfa5bc9b7b574698d7d2b1a60
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-10-28 11:27:54 +0000

    format only

--- spec/haproxy/templates/certs.ttar_spec.rb
+++ spec/haproxy/templates/certs.ttar_spec.rb
@@ -390,4 +390,20 @@ describe 'config/certs.ttar' do
     end
   end
 
-  describe 'ha_proxy.crt_list[].ssl_max_version and ha_proxy.crt_list[].ssl
+  describe 'ha_proxy.crt_list[].ssl_max_version and ha_proxy.crt_list[].ssl_max_version' do
+    let(:ttar) do
+      template.render({
+        'ha_proxy' => {
+          'crt_list' => [{
+            'ssl_min_version' => 'TLSv1.2',
+            'ssl_max_version' => 'TLSv1.2',
+            'ssl_pem' => 'ssl_pem contents'
+          }]
+        }
+      })
+    end
+
+    it 'is included in the crt list' do
+      expect(ttar_entry(ttar, '/var/vcap/jobs/haproxy/config/ssl/crt-list')).to eq(<<~EXPECTED)
+
+        /var/vcap/jo

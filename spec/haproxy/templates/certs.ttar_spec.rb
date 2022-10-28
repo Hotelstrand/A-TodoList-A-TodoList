d6a9415ca57ddb894f45d8d5843374d1f0c9cffd
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-10-28 15:04:45 +0000

    SCSI: megaraid: Use resource_size_t for PCI resources, not long

--- spec/haproxy/templates/certs.ttar_spec.rb
+++ spec/haproxy/templates/certs.ttar_spec.rb
@@ -406,4 +406,24 @@ describe 'config/certs.ttar' do
     it 'is included in the crt list' do
       expect(ttar_entry(ttar, '/var/vcap/jobs/haproxy/config/ssl/crt-list')).to eq(<<~EXPECTED)
 
-        /var/vcap/jo
+        /var/vcap/jobs/haproxy/config/ssl/cert-0.pem [ssl-min-ver TLSv1.2 ssl-max-ver TLSv1.2]
+
+
+      EXPECTED
+    end
+  end
+
+  describe 'ha_proxy.crt_list[].alpn only h2' do
+    let(:ttar) do
+      template.render({
+        'ha_proxy' => {
+          'crt_list' => [{
+            'alpn' => ['h2'],
+            'ssl_pem' => 'ssl_pem contents'
+          }]
+        }
+      })
+    end
+
+    it 'is included in the crt list' do
+      expect(ttar_entry(

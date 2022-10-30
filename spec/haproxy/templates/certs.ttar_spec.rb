45d627aad58b2ba33a2d853150f85dc5ffe84875
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-10-30 10:24:31 +0000

    Config updated

--- spec/haproxy/templates/certs.ttar_spec.rb
+++ spec/haproxy/templates/certs.ttar_spec.rb
@@ -442,4 +442,24 @@ describe 'config/certs.ttar' do
           'crt_list' => [{
             'alpn' => ['h2', 'http/1.1'],
             'ssl_pem' => 'ssl_pem contents'
-      
+          }]
+        }
+      })
+    end
+
+    it 'is included in the crt list' do
+      expect(ttar_entry(ttar, '/var/vcap/jobs/haproxy/config/ssl/crt-list')).to eq(<<~EXPECTED)
+
+        /var/vcap/jobs/haproxy/config/ssl/cert-0.pem [alpn h2,http/1.1]
+
+
+      EXPECTED
+    end
+  end
+
+  describe 'ha_proxy.crt_list[].alpn prefer http/1.1 to h2' do
+    let(:ttar) do
+      template.render({
+        'ha_proxy' => {
+          'crt_list' => [{
+            'alpn' => ['http/1.1', 'h

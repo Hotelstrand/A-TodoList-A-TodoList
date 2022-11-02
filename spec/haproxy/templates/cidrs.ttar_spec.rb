7a176654f9444affd51b94b3c990dec5c0773d3b
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-11-02 22:21:45 +0000

    Fix some code style issues

--- spec/haproxy/templates/cidrs.ttar_spec.rb
+++ spec/haproxy/templates/cidrs.ttar_spec.rb
@@ -3,4 +3,22 @@
 require 'rspec'
 
 describe 'config/cidrs.ttar' do
-  let(:template) { haproxy_job.temp
+  let(:template) { haproxy_job.template('config/cidrs.ttar') }
+
+  describe 'ha_proxy.cidrs_in_file' do
+    let(:ttar) do
+      template.render({
+        'ha_proxy' => {
+          'cidrs_in_file' => [{
+            'cidrs' => [
+              '5.22.1.3',
+              '5.22.12.3'
+            ],
+            'name' => 'sample_cidrs'
+          }]
+        }
+      })
+    end
+
+    it 'has the correct contents' do
+      expect(ttar_entry(ttar, '/var/

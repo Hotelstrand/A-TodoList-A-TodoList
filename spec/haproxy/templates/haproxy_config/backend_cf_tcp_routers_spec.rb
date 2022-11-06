4c1fca5cd26c169d2dfadb98c9ad02ea5df8b20d
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-11-06 17:31:01 +0000

    Linux 3.4.66

--- spec/haproxy/templates/haproxy_config/backend_cf_tcp_routers_spec.rb
+++ spec/haproxy/templates/haproxy_config/backend_cf_tcp_routers_spec.rb
@@ -19,4 +19,23 @@ describe 'config/haproxy.config backend cf_tcp_routers' do
   let(:properties) { {} }
 
   it 'has the correct mode' do
-    expect(backend_cf_tcp_routers).to include('mod
+    expect(backend_cf_tcp_routers).to include('mode tcp')
+  end
+
+  it 'has a healthcheck' do
+    expect(backend_cf_tcp_routers).to include('option httpchk GET /health')
+  end
+
+  context 'when a custom ha_proxy.tcp_backend_config is provided' do
+    let(:properties) do
+      {
+        'tcp_backend_config' => 'custom backend config'
+      }
+    end
+
+    it 'is included in the backend configuration' do
+      expect(backend_cf_tcp_routers).to include('custom backend config')
+    end
+  end
+
+  it 'has the co

728350a04f2cc1f4894dcbde8eea86efcbc0497d
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-11-06 15:23:45 +0000

    moving global vars in f2_reader to struct

--- spec/haproxy/templates/haproxy_config/backend_cf_tcp_routers_spec.rb
+++ spec/haproxy/templates/haproxy_config/backend_cf_tcp_routers_spec.rb
@@ -4,4 +4,19 @@ require 'rspec'
 
 describe 'config/haproxy.config backend cf_tcp_routers' do
   let(:tcp_router_link) do
-    Bosh::Template::Test::Lin
+    Bosh::Template::Test::Link.new(
+      name: 'tcp_router',
+      instances: [Bosh::Template::Test::LinkInstance.new(address: 'tcp.cf.com')]
+    )
+  end
+
+  let(:haproxy_conf) do
+    parse_haproxy_config(template.render({ 'ha_proxy' => properties }, consumes: [tcp_router_link]))
+  end
+
+  let(:backend_cf_tcp_routers) { haproxy_conf['backend cf_tcp_routers'] }
+
+  let(:properties) { {} }
+
+  it 'has the correct mode' do
+    expect(backend_cf_tcp_routers).to include('mod

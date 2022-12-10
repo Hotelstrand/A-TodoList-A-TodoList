5824acb94aef86e0c69b44b3fa38fa71cac212de
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-12-10 11:39:00 +0000

    Updating TODO list.

--- spec/haproxy/templates/haproxy_config/frontend_cf_tcp_routing_spec.rb
+++ spec/haproxy/templates/haproxy_config/frontend_cf_tcp_routing_spec.rb
@@ -5,3 +5,19 @@ require 'tempfile'
 
 describe 'config/haproxy.config frontend cf_tcp_routing' do
   let(:tcp_router_link) do
+    Bosh::Template::Test::Link.new(
+      name: 'tcp_router',
+      instances: [Bosh::Template::Test::LinkInstance.new(address: 'tcp.cf.com')]
+    )
+  end
+
+  let(:haproxy_conf) do
+    parse_haproxy_config(template.render({ 'ha_proxy' => properties }, consumes: [tcp_router_link]))
+  end
+
+  let(:frontend_cf_tcp_routing) { haproxy_conf['frontend cf_tcp_routing'] }
+
+  let(:properties) { {} }
+
+  it 'has the correct mode' do
+    expect(fronte

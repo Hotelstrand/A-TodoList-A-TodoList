bb461e25cf8c0f215a359d05d8a5cb43d0b78f62
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-12-12 08:31:40 +0000

    Update rutas.md

--- spec/haproxy/templates/haproxy_config/frontend_http_spec.rb
+++ spec/haproxy/templates/haproxy_config/frontend_http_spec.rb
@@ -4,4 +4,23 @@ require 'rspec'
 
 describe 'config/haproxy.config HTTP frontend' do
   let(:haproxy_conf) do
-    parse_haproxy_config(template.render({ 'ha_proxy' => properties 
+    parse_haproxy_config(template.render({ 'ha_proxy' => properties }))
+  end
+
+  let(:frontend_http) { haproxy_conf['frontend http-in'] }
+  let(:properties) { {} }
+
+  it 'binds to all interfaces by default' do
+    expect(frontend_http).to include('bind :80')
+  end
+
+  context 'when ha_proxy.binding_ip is provided' do
+    let(:properties) do
+      { 'binding_ip' => '1.2.3.4' }
+    end
+
+    it 'binds to the provided ip' do
+      expect(frontend_http).to include('bind 1.2.3.4:80')
+    end
+
+    co

9d083f4c07bb2427ec528ed9db3ab31e143b991c
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2022-12-22 05:33:37 +0000

    update test data

--- spec/haproxy/templates/haproxy_config/global_and_default_options_spec.rb
+++ spec/haproxy/templates/haproxy_config/global_and_default_options_spec.rb
@@ -5,4 +5,20 @@ require 'haproxy-tools'
 
 describe 'config/haproxy.config global and default options' do
   let(:haproxy_conf) do
-    parse_haproxy_config(temp
+    parse_haproxy_config(template.render({ 'ha_proxy' => properties }))
+  end
+
+  let(:global) { haproxy_conf['global'] }
+  let(:defaults) { haproxy_conf['defaults'] }
+
+  let(:properties) { {} }
+
+  it 'renders a valid haproxy template' do
+    expect do
+      HAProxy::Parser.new.parse(template.render({}))
+    end.not_to raise_error
+  end
+
+  it 'has expected defaults' do
+    expect(defaults).to include('log global')
+    exp

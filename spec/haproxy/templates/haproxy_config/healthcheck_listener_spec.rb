9cbf0895abce23600c5259ff89786e6f9a4465fd
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2023-01-07 17:15:42 +0000

    removed unnecessary function

--- spec/haproxy/templates/haproxy_config/healthcheck_listener_spec.rb
+++ spec/haproxy/templates/haproxy_config/healthcheck_listener_spec.rb
@@ -8,4 +8,16 @@ describe 'config/haproxy.config healthcheck listeners' do
   end
 
   context 'when ha_proxy.enable_health_check_http is true' do
-    let
+    let(:healthcheck_listener) { haproxy_conf['listen health_check_http_url'] }
+
+    let(:properties) do
+      {
+        'enable_health_check_http' => true
+      }
+    end
+
+    it 'adds a health check listener for the http-routers-http1' do
+      expect(healthcheck_listener).to include('bind :8080')
+      expect(healthcheck_listener).to include('mode http')
+      expect(healthcheck_listener).to include('option httpclose')
+      expect

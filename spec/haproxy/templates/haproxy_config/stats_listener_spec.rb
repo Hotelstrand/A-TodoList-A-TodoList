42e5c8e64b3537accffc5fcc44f5fabce30bb9f8
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2023-02-08 13:51:08 +0000

    Address review feedback.

--- spec/haproxy/templates/haproxy_config/stats_listener_spec.rb
+++ spec/haproxy/templates/haproxy_config/stats_listener_spec.rb
@@ -7,4 +7,20 @@ describe 'config/haproxy.config stats listener' do
     parse_haproxy_config(template.render({ 'ha_proxy' => properties }))
   end
 
-  context 'whe
+  context 'when ha_proxy.stats_enable is true' do
+    let(:default_properties) do
+      {
+        'syslog_server' => '/dev/log',
+        'stats_enable' => true,
+        'stats_user' => 'admin',
+        'stats_password' => 'secret',
+        'stats_uri' => 'foo'
+      }
+    end
+
+    let(:properties) { default_properties }
+
+    let(:stats_listener) { haproxy_conf['listen stats'] }
+
+    it 'sets up a stats listener for each process' do
+      expect(stats_listener).to inclu

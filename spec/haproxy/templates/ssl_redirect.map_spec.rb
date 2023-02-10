2dd8c27303e1a056363f722e58c643a80b7f5104
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2023-02-10 08:28:41 +0000

    typo2

--- spec/haproxy/templates/ssl_redirect.map_spec.rb
+++ spec/haproxy/templates/ssl_redirect.map_spec.rb
@@ -5,4 +5,24 @@ require 'rspec'
 describe 'config/ssl_redirect.map' do
   let(:template) { haproxy_job.template('config/ssl_redirect.map') }
 
-  context 'when ha_proxy.https_redir
+  context 'when ha_proxy.https_redirect_domains is provided' do
+    it 'has the correct contents' do
+      expect(template.render({
+        'ha_proxy' => {
+          'https_redirect_domains' => [
+            'google.com',
+            'bing.com'
+          ]
+        }
+      })).to eq(<<~EXPECTED)
+
+        google.com	true
+
+        bing.com	true
+
+      EXPECTED
+    end
+  end
+
+  context 'when ha_proxy.https_redirect_domains is not provided' do
+  

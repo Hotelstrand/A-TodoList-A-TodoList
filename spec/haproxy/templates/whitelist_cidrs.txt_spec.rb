598f796835bc7bba0800382713af4f47d585797c
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2023-02-16 20:42:54 +0000

    Updated submodule

--- spec/haproxy/templates/whitelist_cidrs.txt_spec.rb
+++ spec/haproxy/templates/whitelist_cidrs.txt_spec.rb
@@ -7,4 +7,16 @@ describe 'config/whitelist_cidrs.txt' do
 
   context 'when ha_proxy.cidr_whitelist is provided' do
     context 'when an array of cidrs is provided' do
- 
+      it 'has the correct contents' do
+        expect(template.render({
+          'ha_proxy' => {
+            'cidr_whitelist' => [
+              '10.0.0.0/8',
+              '192.168.2.0/24'
+            ]
+          }
+        })).to eq(<<~EXPECTED)
+          # generated from whitelist_cidrs.txt.erb
+
+          # BEGIN whitelist cidrs
+          # detected cidrs provided as array in cleartext format

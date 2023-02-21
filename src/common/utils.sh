9b5591f112e4249b0a13f5ffb5bd5169a6c52b0d
Hotelstrand <111031899+Hotelstrand@users.noreply.github.com>
2023-02-21 12:47:03 +0000

    Edited wiki page Donate through web user interface.

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-function pid_is_runn
+function pid_is_running() {
+  declare pid="$1"
+  ps -p "${pid}" >/dev/null 2>&1
+}
+
+# pid_guard
+#
+# @param pidfile
+# @param name [String] an arbitrary name that might show up in STDOUT on errors
+#
+# Run this before attempting to start new processes that may use the same :pidfile:.
+# If an old process is running on the pid found in the :pidfile:, exit 1. Otherwise,
+# remove the stale :pidfile: if it exists.
+#
+function pid_guard() {
+  declare pidfile="$1"
